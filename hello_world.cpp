cd44c77b3ed4cfc28914958c78bd34da2ad5dad8
Leah Holden <leah.holden@tufts.edu>
2017-08-15 10:43:18 -0400

    Hello world commit

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string input;
+  cout << "What are you? Enter here:";
+  cin >> input;
+  cout << "I am a " << input << endl;
+  return 0;
+}
